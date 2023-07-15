79d3369638e55150326a725e6c3cce6c428b244a
AziiQ <120739025+AziiQ@users.noreply.github.com>
2023-07-15 23:57:00 +0100

    Add files via upload

---
+++ contact-form/config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'subject' => [
+        'prefix' => '[Contact Form]'
+    ],
+    'emails' => [
+        'to'   => 'matthew.g.a.1996@gmail.com',
+        'from' => 'https://aziiq.github.io/Portfolio/'
+    ],
+    'messages' => [
+        'error'   => 'There was an error sending your message, please try again later.',
+        'success' => 'Your message has been sent successfully.'
+    ],
+    'fields' => [
+        'name'     => 'Name',
+        'email'    => 'Email',
+        'subject'  => 'Subject',
+        'message'  => 'Message',
+        'btn-send' => 'Send'
+    ]
+];
